1dadfe1f49852dc3542d6dfedb5c3b538e5b7b01
fujia <5211666@qq.com>
2016-11-02 23:38:57 +0800

    gg

--- release/app/viewmodels/editmr.js
+++ release/app/viewmodels/editmr.js
@@ -11,7 +11,6 @@ define(['durandal/app','knockout','plugins/router','plugins/dialog','calendar/fu
         var dbs=new dbHelper(baseUrl,user,ucode);
         var resid=appConfig.meetingroom.resid;
         var cmswhere="mid="+mid;
-        console.log(cmswhere);
         dbs.dbGetdata(resid,0,cmswhere,fnSuccess,null,null);
         function fnSuccess(data,subdata){
             mini.parse();
@@ -26,6 +25,10 @@ define(['durandal/app','knockout','plugins/router','plugins/dialog','calendar/fu
                 var img=$("#imgUploaded");
                 img[0].src=imgurl;
             }   
+        }
+        cencelClick=function(){
+            $('.modalBlockout').remove();
+            $('#editform').remove();
         }
         saveClick=function(){
             mini.parse();
@@ -46,9 +49,36 @@ define(['durandal/app','knockout','plugins/router','plugins/dialog','calendar/fu
                 dialog.showMessage(text,'修改失败',['返回'],true);
             }
             function fnhttperror(jqXHR, textStatus, errorThrown){
-                dialog.showMessage('error','会议室新增',['返回'],true);
+                dialog.showMessage('error','错误',['返回'],true);
             }
         }
+        delClick=function(){
+            if(confirm('您确定要删除么？')){
+                mini.parse();
+                var form = new mini.Form("editform");
+                var o =  new mini.Form("editform").getData();
+                form.validate(); 
+                if (form.isValid() == false) return;
+                o._id=1;
+                o._state="modified";
+                o.remove="Y";
+                var json = mini.encode([o]);
+                dbs.dbSavedata(resid,0,json,dataSaved,fnerror,fnhttperror);
+                function dataSaved(text){
+                    dialog.showMessage('<h1>删除成功</h1>','会议室新增',['返回'],true);
+                    $('.modalBlockout').remove();
+                    $('#editform').remove();
+                }
+                function fnerror(text){
+                    dialog.showMessage(text,'删除失败',['返回'],true);
+                }
+                function fnhttperror(jqXHR, textStatus, errorThrown){
+                    dialog.showMessage('error','错误',['返回'],true);
+                }
+            }else{
+                return;
+            }
+        };
     };
     editmr.show = function(mdata){
         mid=mdata;
@@ -59,58 +89,4 @@ define(['durandal/app','knockout','plugins/router','plugins/dialog','calendar/fu
            
        
     return editmr;
-    // return {
-    //     editmr,
-    //     activate:function(){},
-    //     attached:function(){
-            
-    //         baseUrl=appConfig.app.baseUrl;
-    //         var ucode = appConfig.app.ucode;
-    //         var user  = appConfig.app.user;
-    //         var dbs=new dbHelper(baseUrl,user,ucode);
-    //         var resid=appConfig.meetingroom.resid;
-    //         mini.parse();
-
-    //         var fileupload = mini.get("fileupload1");
-    //         fileupload.setUploadUrl(appConfig.app.uploadFileUrl+"?savepath=e:\\web\\rispweb\\upfiles&httppath="+appConfig.app.httppath);
-    //         var imgfield=mini.get('imgurl');
-    //         var imgurl= imgfield.getValue();
-    //         if (imgurl)
-    //         {
-    //             var img=$("#imgUploaded");
-    //             img[0].src=imgurl;
-    //         }
-
-
-    //         saveClick=function(){
-    //                 mini.parse();
-    //                 var form = new mini.Form("form");
-    //                 var o =  new mini.Form("form").getData();
-    //                 form.validate(); 
-    //                 if (form.isValid() == false) return;
-    //                 o._id=1;
-    //                 o._state="added";
-    //                 var json = mini.encode([o]);
-    //                 dbs.dbSavedata(resid,0,json,dataSaved,fnerror,fnhttperror);
-    //                 function dataSaved(text){
-    //                     dialog.showMessage('<h1>添加成功</h1>','会议室新增',['返回'],true);
-    //                     $('#imgurl').val("");
-    //                 }
-    //                 function fnerror(text){
-    //                     dialog.showMessage('<h1>添加失败</h1>','会议室新增',['返回'],true);
-    //                     $('.mini-textbox').attr("value","");
-    //                     console.log(text);
-    //                 }
-    //                 function fnhttperror(jqXHR, textStatus, errorThrown){
-    //                     dialog.showMessage('error','会议室新增',['返回'],true);
-    //                     $('.mini-textbox').attr("value","");
-    //                 }
-    //                 $('input[name="tel"]').val("");
-    //                 $('input[name="mname"]').val("");
-    //                 $('input[name="mrimage"]').val("");
-    //                 $('#imgUploaded').attr("src","");
-
-    //         }  
-    //     }
-    // }
 })
